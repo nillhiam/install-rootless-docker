d5efe0699a40dc9429969699849aca18c2db4859
nillhiam <nillhiam@gmail.com>
2025-04-06 02:37:05 +0000

    ci: uninstall docker

---
+++ uninstall-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: uninstall-docker.sh
+# 
+#         USAGE: ./uninstall-docker.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 04/06/2025 01:46
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# Uninstall the Docker Engine, CLI, containerd, and Docker Compose packages
+sudo apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+# Remove source list and keyrings
+sudo rm /etc/apt/sources.list.d/docker.list
+sudo rm /etc/apt/keyrings/docker.asc
+
