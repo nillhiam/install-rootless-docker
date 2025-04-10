14d53fe162b176dcde36d743c94981f0e3db11dc
nillhiam <nillhiam@gmail.com>
2025-04-10 17:13:02 +0000

    ci: uninstall docker

--- uninstall-docker.sh
+++ uninstall-docker.sh
@@ -26,3 +26,5 @@ sudo apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 sudo rm /etc/apt/sources.list.d/docker.list
 sudo rm /etc/apt/keyrings/docker.asc
 
+# Update packages
+sudo apt-get update
